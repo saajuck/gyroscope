1d220061304f069e61eb852a353b97d782d7a44e
Benoît <benoit.delamaire@gmail.com>
2023-01-29 15:59:20 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import './style.css'
 import { setup } from './setup'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
     <video controls id="video">
 
      <source src="" type="video/mp4" id="source">
 
     </video>
     <p id="txt"></p>
-  </div>
 `
 
 setup()
--- src/setup.ts
+++ src/setup.ts
@@ -1,21 +1,16 @@
 import video from './assets/vanhelsing.mp4'
 
 export const setup = () => {
-    const htmlVideo = document.querySelector<HTMLButtonElement>('#video')!
+    const htmlVideo = document.querySelector<HTMLVideoElement>('#video')!
     const htmlText = document.querySelector<HTMLButtonElement>('#txt')!
     const el = htmlVideo.children[0]
     el.setAttribute('src', video)
-    // let gyroscope = new Gyroscope({frequency: 60});
-    // console.log(gyroscope)
-    // gyroscope.addEventListener('reading', (e) => {
-    //   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-    //   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-    //   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-    // });
-    // gyroscope.addEventListener('error', (e) => {
-    //     console.log(e)
-    // })
-    // gyroscope.start();
+    htmlVideo.addEventListener('loadedmetadata', () => {
+        console.log(htmlVideo.duration)
+        htmlVideo.currentTime = 0.975;
+
+    });
+       
 
     const handleOrientation = (event: DeviceOrientationEvent)=>{
         let x = event.beta; // In degree in the range [-180,180)
