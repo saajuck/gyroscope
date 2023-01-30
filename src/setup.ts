0f304fe6bc2bea5b78bd9d4d8319d752a74dc721
Benoît <benoit.delamaire@gmail.com>
2023-01-30 20:05:00 +0100

    first try

--- src/setup.ts
+++ src/setup.ts
@@ -1,27 +1,33 @@
 import videoMp4 from './assets/vanhelsing.mp4'
 import videoWebm from './assets/vanhelsing.webm'
 
+const calcAngleDegrees = (x:number, y:number) => Math.atan2(y, x) * 180 / Math.PI;
+
+  
 export const setup = () => {
     const htmlVideo = document.querySelector<HTMLVideoElement>('#video')!
     const htmlText = document.querySelector<HTMLButtonElement>('#txt')!
+
+    const handleOrientation = (event: DeviceOrientationEvent)=>{
+        const y = event.beta; 
+        const x = event.gamma;
+      
+        htmlText.textContent = `beta : ${Math.round(Number(x) * 100) / 100}\n`;
+        htmlText.textContent += `gamma: ${Math.round(Number(y) * 100) / 100}\n`;
+        const degres = calcAngleDegrees(x || 0, y || 0)
+        htmlVideo.currentTime = degres * htmlVideo.currentTime / 360;
+    }
+
+
     htmlVideo.children[0].setAttribute('src', videoMp4)
     htmlVideo.children[1].setAttribute('src', videoWebm)
     htmlVideo.addEventListener('loadedmetadata', () => {
         console.log(htmlVideo.duration)
         htmlVideo.currentTime = 0.975;
-
+        window.addEventListener("deviceorientation", handleOrientation);
     });
        
 
-    const handleOrientation = (event: DeviceOrientationEvent)=>{
-        let x = event.beta; // In degree in the range [-180,180)
-        let y = event.gamma; // In degree in the range [-90,90)
-      
-        htmlText.textContent = `beta : ${Math.round(Number(x) * 100) / 100}\n`;
-        htmlText.textContent += `gamma: ${Math.round(Number(y) * 100) / 100}\n`;
-    }
-
-    window.addEventListener("deviceorientation", handleOrientation);
 }
 
 
