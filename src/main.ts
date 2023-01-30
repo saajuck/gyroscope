afa0b729cc6eb3cbaee9d9a7a0c7773189bafcbd
Benoît <benoit.delamaire@gmail.com>
2023-01-30 20:38:06 +0100

    ghmmmm

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import { setup } from './setup'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <video autobuffer control id="video">
+    <video autobuffer controls id="video">
 
      <source id="sourceMp4" src="" type="video/mp4" id="source">
      <source id="sourceWebm" src="" type="video/webm" id="source">
