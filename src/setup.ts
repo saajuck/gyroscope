c8335f297f842226f3e2a004b141f1f04725af65
Benoît <benoit.delamaire@gmail.com>
2023-02-05 23:06:43 +0100

    wip

--- src/setup.ts
+++ src/setup.ts
@@ -10,10 +10,10 @@ export const setup = () => {
     htmlPerso.style.transform = `translate(${getTransform(90 * 0.1, -20 * 0.1)})`;
 
     const handleOrientation = (event: DeviceOrientationEvent)=>{
-        let x = event.beta || 0; // In degree in the range [-180,180)
-        let y = event.gamma || 0; // In degree in the range [-90,90)
-        htmlPerso.style.transform = `translate(${getTransform(x * 0.1, y * 0.1)})`;
-        htmlFg.style.transform = `translate(${getTransform(x * 0.2, y * 0.2)})`;
+        let y = event.beta || 0; // In degree in the range [-180,180)
+        let x = event.gamma || 0; // In degree in the range [-90,90)
+        htmlPerso.style.transform = `translate(${getTransform(x * 0.5, y * 0.5)})`;
+        htmlFg.style.transform = `translate(${getTransform(x * 0.8, y * 0.8)})`;
 
         htmlText.textContent = `beta : ${Math.round(Number(x) * 100) / 100}\n`;
         htmlText.textContent += `gamma: ${Math.round(Number(y) * 100) / 100}\n`;
