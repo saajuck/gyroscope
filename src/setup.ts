7bc80803734485238fafb4cebe402ac1457b98a7
Benoît <benoit.delamaire@gmail.com>
2023-02-05 23:43:34 +0100

    again

--- src/setup.ts
+++ src/setup.ts
@@ -17,7 +17,8 @@ const queryPermissions = async():Promise<'granted' | 'denied'> => {
         return 'granted'
     }
   }
-  
+ 
+  const formatPosition = (value:number, ponderation:number):string => value * ponderation + 50 +'%'
 
 export const setup = () => {
     const htmlPerso = document.querySelector<HTMLVideoElement>('.perso')!
@@ -28,10 +29,10 @@ export const setup = () => {
     const handleOrientation = (event: DeviceOrientationEvent)=>{
         let y = event.beta || 0; // In degree in the range [-180,180)
         let x = event.gamma || 0; // In degree in the range [-90,90)
-        htmlPerso.style.top = `${(y + 50) * 0.05}%`;
-        htmlPerso.style.left = `${x + 50* 0.05}%`;
-        htmlFg.style.top = `${y+ 50 * 0.07}%`;
-        htmlFg.style.left = `${x+ 50 * 0.07}%`;
+        htmlPerso.style.top = formatPosition(y, 0.05);
+        htmlPerso.style.left = formatPosition(x, 0.05);
+        htmlFg.style.top = formatPosition(y, 0.07);
+        htmlFg.style.left = formatPosition(x, 0.07);
 
         htmlText.textContent = `beta : ${Math.round(Number(x) * 100) / 100}\n`;
         htmlText.textContent += `gamma: ${Math.round(Number(y) * 100) / 100}\n`;
