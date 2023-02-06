ac1db7d00c6a9fd44ec0e5052bf9f61f8cb3b8dd
Benoît <benoit.delamaire@gmail.com>
2023-02-06 12:38:09 +0100

    test

--- src/setup.ts
+++ src/setup.ts
@@ -5,42 +5,46 @@ interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
 const requestPermission = (DeviceOrientationEvent as unknown as DeviceOrientationEventiOS).requestPermission;
 const iOS = typeof requestPermission === 'function';
   
-const queryPermissions = async():Promise<'granted' | 'denied'> => {
-    if(iOS && requestPermission) {
-    try {
-        const permissionState =  await requestPermission();
-        return permissionState
-      } catch(e) {
-       return "denied";
-      }
-    }else{
-        return 'granted'
-    }
-  }
- 
-  const formatPosition = (value:number, ponderation:number):string => value * ponderation + 50 +'%'
+const formatPosition = (value:number, ponderation:number):string => value * ponderation + 50 +'%'
 
-export const setup = () => {
-    const htmlPerso = document.querySelector<HTMLVideoElement>('.perso')!
-    // const htmlBg = document.querySelector<HTMLVideoElement>('.bg')!
-    const htmlFg = document.querySelector<HTMLVideoElement>('.fg')!
+const handleOrientation = (event: DeviceOrientationEvent)=>{
+    const htmlPerso = document.querySelector<HTMLImageElement>('.perso')!
+    const htmlFg = document.querySelector<HTMLImageElement>('.fg')!
     const htmlText = document.querySelector<HTMLButtonElement>('#txt')!
-     
-    const handleOrientation = (event: DeviceOrientationEvent)=>{
-        let y = event.beta || 0; // In degree in the range [-180,180)
-        let x = event.gamma || 0; // In degree in the range [-90,90)
-        htmlPerso.style.top = formatPosition(y, 0.1);
-        htmlPerso.style.left = formatPosition(x, 0.1);
-        htmlFg.style.top = formatPosition(y, 0.15);
-        htmlFg.style.left = formatPosition(x, 0.15);
+    let y = event.beta || 0; // In degree in the range [-180,180)
+    let x = event.gamma || 0; // In degree in the range [-90,90)
+    htmlPerso.style.top = formatPosition(y, 0.1);
+    htmlPerso.style.left = formatPosition(x, 0.1);
+    htmlFg.style.top = formatPosition(y, 0.15);
+    htmlFg.style.left = formatPosition(x, 0.15);
 
-        htmlText.textContent = `beta : ${Math.round(Number(x) * 100) / 100}\n`;
-        htmlText.textContent += `gamma: ${Math.round(Number(y) * 100) / 100}\n`;
-    }
-    queryPermissions().then(res =>{
-        if(res ==='granted' ){
-            window.addEventListener("deviceorientation", handleOrientation);
+    htmlText.textContent = `beta : ${Math.round(Number(x) * 100) / 100}\n`;
+    htmlText.textContent += `gamma: ${Math.round(Number(y) * 100) / 100}\n`;
+}
+
+const queryPermissions = async():Promise<void> => {
+    if(iOS && requestPermission) {
+        try {
+            const permissionState =  await requestPermission();
+            if(permissionState === 'granted'){
+                window.addEventListener("deviceorientation", handleOrientation);
+            }
+        } catch(e) {
+            const htmlText = document.querySelector<HTMLButtonElement>('#txt')!
+            htmlText.textContent = 'permission not granted'
         }
-    })
+    }else{
+        window.addEventListener("deviceorientation", handleOrientation);
+    }
 }
  
+export const setup = () => {
+    const htmlBody = document.querySelector<HTMLBodyElement>('body')!
+    console.log(htmlBody)
+    if(iOS) { 
+        console.log(htmlBody)
+        htmlBody.addEventListener('onClick', queryPermissions)
+    }else{
+        window.addEventListener("deviceorientation", handleOrientation);
+    }
+}
